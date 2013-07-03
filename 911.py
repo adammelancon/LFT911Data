b45b7e17754a4978f87c077458631fd61f3487cf
adammelancon <adammelancon@gmail.com>
2013-07-03 16:56:15 -0500

    Create 911.py

---
+++ 911.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+#this url is the middle frame with the incident list at lafayette911.org
+url = "http://67.32.159.27/webcad/webcad.asp"
+page = urllib2.urlopen(url)
+soup = BeautifulSoup(page.read())
+
+#test for now to just find links since incidents are down at the moment
+incidents = soup.find_all('a')
+
+for links in incidents:
+  print(links.get('href'))
+
+
