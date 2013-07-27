20c5510c42e908fadc466cb49d522028f2fe7c41
Adam Melancon <adammelancon@gmail.com>
2013-07-27 01:26:17 -0500

    Need to figure out how to remove "Â" on output to a text file.

--- 911.py
+++ 911.py
@@ -1,22 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #testing get commit from raspi
 # Testing 123
 
 from bs4 import BeautifulSoup
 import urllib2
+import codecs
 
 #this url is the middle frame with the incident list at lafayette911.org
 url = "http://67.32.159.27/webcad/webcad.asp"
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page.read())
 
-# Create an emapty list for incidents
-incidents = []
+def get_incidents():
+    # Create an emapty list for incidents
+    incidents = []
+    # Find the second table which has the incidents and find the individual rows.
+    for i in soup.findAll('table')[1].findAll('tr'):
+        incidents.append(i.get_text()) # Add the text from the row to the list
 
-# Find the second table which has the incidents and find the individual rows.
-for i in soup.findAll('table')[1].findAll('tr'):
-    incidents.append(i.get_text()) # Add the text from the row to the list
+    # Print each line of the list on its own.
+    #for i in incidents:
+    #    print i
+    #    print "=============================="
+    #print incidents
+    write_file(incidents)
 
-# Print each line of the list on its own.
-for i in incidents:
-   print i
-   print "=============================="
+def write_file(incidents):
+    file = codecs.open("/var/www/911.txt", "w", encoding="UTF-8")
+    incidents2 = []
+    print incidents
+    for i in incidents:
+        i.replace(u"Â ", "")
+        incidents2.append(i)
+    print incidents2
+    for i in incidents2:
+        file.write(i + "\n")
+    #for i in incidents:
+    #    file.write(i + "\n")
+    #    print i
+    file.close()
+
+get_incidents()
+
